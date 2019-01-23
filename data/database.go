7c74b0d0d1f3874b6c4475442d1f7d381504397d
imaishiuta <shuta25le0n@gmail.com>
2019-01-23 19:41:18 +0900

    edit data package

---
+++ data/database.go
@@ -0,0 +1,105 @@
+package data
+
+import (
+  "fmt"
+  "github.com/gin-gonic/gin"
+  "github.com/gin-gonic/contrib/sessions"
+  "github.com/jinzhu/gorm"
+)
+
+type User struct {
+  gorm.Model
+  Name string
+  Email string
+  Password string
+  Confirm_Password string
+  Messages []Message
+}
+
+type Message struct {
+  gorm.Model
+  Text string `json:"name"`
+  UserId uint
+  User User
+}
+
+
+func CreateDB() {
+  db, err := gorm.Open("mysql", "root@/chatapp?charset=utf8&parseTime=True&loc=Local")
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer db.Close()
+  db.AutoMigrate(&Message{}, &User{})
+  db.Model(&Message{}).Related(&User{})
+}
+
+func Create_text(c *gin.Context, text string) {
+  db, err := gorm.Open("mysql", "root@/chatapp?charset=utf8&parseTime=True&loc=Local")
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer db.Close()
+  session := sessions.Default(c)
+  user_id := session.Get("userID")
+  fmt.Println(user_id)
+  msg := Message{Text: text, UserId: user_id.(uint)}
+  db.Create(&msg)
+}
+
+func Create_user(name string, email string, password string, confirm_password string) {
+  db, err := gorm.Open("mysql", "root@/chatapp?charset=utf8&parseTime=True&loc=Local")
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer db.Close()
+  user := User{Name: name,
+  Email: email,
+  Password: password,
+  Confirm_Password: confirm_password,
+  }
+  db.Create(&user)
+}
+
+func Find_user(email string, password string) User{
+  db, err := gorm.Open("mysql", "root@/chatapp?charset=utf8&parseTime=True&loc=Local")
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer db.Close()
+  var user User
+  db.Where("email = ? AND password = ?", email, password).Find(&user)
+  return user
+}
+
+func Get_All_Messages() []Message{
+  db, err := gorm.Open("mysql", "root@/chatapp?charset=utf8&parseTime=True&loc=Local")
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer db.Close()
+  var messages []Message
+  var user User
+  db.Find(&messages)
+  db.Model(&user).Related(&messages)
+  db.Preload("User").Find(&messages)
+  return messages
+}
+
+func Get_Current_User(c *gin.Context) User {
+  db, err := gorm.Open("mysql", "root@/chatapp?charset=utf8&parseTime=True&loc=Local")
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer db.Close()
+  session := sessions.Default(c)
+  user_id := session.Get("userID")
+  var user User
+  var messages []Message
+  db.Where("id = ?", user_id).Find(&user)
+  db.Model(&user).Related(&messages)
+  db.Find(&user).Related(&user.Messages)
+  fmt.Println(user)
+  return user
+}
+
