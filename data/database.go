6ba0f56fa478143caf4b6231a9b1019fd6e1803f
imaishiuta <shuta25le0n@gmail.com>
2019-01-29 23:26:10 +0900

    Edit database.go
    add comment struct
    define Get_Message function to get specific message by message_id
    define Post_Comment to Post comment to the message

--- data/database.go
+++ data/database.go
@@ -23,6 +23,14 @@ type Message struct {
   User User
 }
 
+type Comment struct {
+  gorm.Model
+  Text string `json:"name"`
+  UserId uint `json:"userid"`
+  MessageId string `json:"messageid"`
+  User User
+  Message Message
+}
 
 func CreateDB() {
   db, err := gorm.Open("mysql", "root@/chatapp?charset=utf8&parseTime=True&loc=Local")
@@ -30,8 +38,7 @@ func CreateDB() {
     fmt.Println(err)
   }
   defer db.Close()
-  db.AutoMigrate(&Message{}, &User{})
-  db.Model(&Message{}).Related(&User{})
+  db.AutoMigrate(&Message{}, &User{}, &Comment{})
 }
 
 func Create_text(c *gin.Context, text string) {
@@ -42,7 +49,6 @@ func Create_text(c *gin.Context, text string) {
   defer db.Close()
   session := sessions.Default(c)
   user_id := session.Get("userID")
-  fmt.Println(user_id)
   msg := Message{Text: text, UserId: user_id.(uint)}
   db.Create(&msg)
 }
@@ -103,3 +109,26 @@ func Get_Current_User(c *gin.Context) User {
   return user
 }
 
+func Get_Message(id string) Message {
+  db, err := gorm.Open("mysql", "root@/chatapp?charset=utf8&parseTime=True&loc=Local")
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer db.Close()
+  var message Message
+  db.First(&message, id).Related(&message.User)
+  fmt.Println(message)
+  return message
+}
+
+func Post_Comment(c *gin.Context ,id string, text string) {
+  db, err := gorm.Open("mysql", "root@/chatapp?charset=utf8&parseTime=True&loc=Local")
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer db.Close()
+  session := sessions.Default(c)
+  user_id := session.Get("userID")
+  comment := Comment{Text: text, UserId: user_id.(uint), MessageId: id}
+  db.Create(&comment)
+}
